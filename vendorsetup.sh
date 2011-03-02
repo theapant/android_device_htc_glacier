206f67fd63b0c3010931f41d308bbbabbd4866e4
andrew.boren <andrew.boren@gmail.com>
2011-03-01 21:09:06 -0700

    glacier: Remove non-evervolv build options.
    Change-Id: Iaaf1888a8f10f204807ac81d85a97cc1dbe11628

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo htc_glacier-userdebug
-add_lunch_combo htc_glacier-eng
+#We're only going to be building Evervolv, so comment this out for now
+#add_lunch_combo htc_glacier-userdebug
+#add_lunch_combo htc_glacier-eng
