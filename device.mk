a59d66f16701b3ffec60c202e0eaa8b15017e2e6
Nick Reuter <nreuter85@gmail.com>
2013-02-23 03:00:46 -0600

    cleanup some stuff from common
    Change-Id: If889fb9a93d9f5219e4ffbe3490fee372a979e3a

--- device.mk
+++ device.mk
@@ -79,7 +79,6 @@ PRODUCT_COPY_FILES += \
 # media config xml file
 PRODUCT_COPY_FILES += \
     device/htc/glacier/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
-    device/htc/glacier/prebuilt/audio_policy.conf:system/etc/audio_policy.conf \
     device/htc/glacier/prebuilt/media_codecs.xml:system/etc/media_codecs.xml	
 
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
