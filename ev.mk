07c75eabfe30a71b1c1ab3837ee431eed35cf2c1
Nick Reuter <nreuter85@gmail.com>
2013-03-21 03:40:11 -0500

    Glacier: update to fingerprint
    Change-Id: I56e67cc7db01f18a0286a8879bb238f6d8183959

--- ev.mk
+++ ev.mk
@@ -13,7 +13,7 @@ PRODUCT_DEVICE := glacier
 PRODUCT_BRAND := T-Mobile
 PRODUCT_MODEL := myTouch 4G
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_glacier BUILD_FINGERPRINT=htc_europe/htc_primou/primou:4.0.3/IML74K/39988.1:user/release-keys PRIVATE_BUILD_DESC="2.19.531.1 CL95282 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_glacier BUILD_FINGERPRINT=tmous/htc_glacier/glacier:4.2.2/JDQ39/95282.1:user/release-keys PRIVATE_BUILD_DESC="2.19.531.1 CL95282 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Glacialis
