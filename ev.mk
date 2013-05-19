1dfd55c68a41bce2c01cebc2b8a121962b2d92ac
Anand Pant <theapant@gmail.com>
2013-05-19 07:24:37 -0500

    liquid specific

--- ev.mk
+++ ev.mk
@@ -2,13 +2,13 @@
 $(call inherit-product, device/htc/glacier/device.mk)
 
 # Inherit some common evervolv stuff.
-$(call inherit-product, vendor/ev/config/gsm.mk)
-$(call inherit-product, vendor/ev/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := ev_glacier
+PRODUCT_NAME := liquid_glacier
 PRODUCT_DEVICE := glacier
 PRODUCT_BRAND := T-Mobile
 PRODUCT_MODEL := myTouch 4G
@@ -18,13 +18,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_glacier BUILD_FINGERPRINT=tmous
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Glacialis
 PRODUCT_VERSION_DEVICE_SPECIFIC := p1
-
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your myTouch 4G\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
-
-# Hot reboot
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/ev/overlay/hot_reboot
-
-# Copy compatible prebuilt files
-PRODUCT_COPY_FILES +=  \
-    vendor/ev/prebuilt/wvga/media/bootanimation.zip:system/media/bootanimation.zip
